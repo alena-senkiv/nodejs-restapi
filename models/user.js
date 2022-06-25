d5dea5274cc3fa97839066b859fe781e4345748b
alena-senkiv <alena.senkiv@gmail.com>
2022-06-25 12:41:03 +0200

    add verification

--- models/user.js
+++ models/user.js
@@ -27,6 +27,15 @@ const userSchema = Schema(
       type: String,
       required: true,
     },
+
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
   { versionKey: false, timestamps: true }
 );
@@ -41,7 +50,9 @@ userSchema.methods.comparePassword = function (password) {
 
 const joiSchema = Joi.object({
   password: Joi.string().min(6).required(),
-  email: Joi.string().required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
   subscription: Joi.string().valid("starter", "pro", "business"),
 });
 
@@ -49,6 +60,17 @@ const subscriptionJoiSchema = Joi.object({
   subscription: Joi.string().valid("starter", "pro", "business").required(),
 });
 
+const sendEmailJoiSchema = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .messages({
+      "string.email": "email should be a type of 'email'",
+      "string.empty": "email cannot be an empty field",
+      "any.required": "missing required field email",
+    })
+    .required(),
+});
+
 const User = model("user", userSchema);
 
-module.exports = { User, joiSchema, subscriptionJoiSchema };
+module.exports = { User, joiSchema, subscriptionJoiSchema, sendEmailJoiSchema };
